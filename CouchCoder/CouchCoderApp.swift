e6eaf138876d51d29d8c1a8a9501cd236557003b
Chase Kunz <chaseokunz@gmail.com>
2025-11-16 10:52:59 -0600

    Initial Commit

---
+++ CouchCoder/CouchCoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouchCoderApp.swift
+//  CouchCoder
+//
+//  Created by Chase Kunz on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CouchCoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CouchCoderTests/CouchCoderTests.swift
@@ -0,0 +1,17 @@
+//
+//  CouchCoderTests.swift
+//  CouchCoderTests
+//
+//  Created by Chase Kunz on 11/16/25.
+//
+
+import Testing
+@testable import CouchCoder
+
+struct CouchCoderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
